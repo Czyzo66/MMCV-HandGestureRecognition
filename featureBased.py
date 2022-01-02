c3d6154d4bd4d3982da1ac47866ced48f3733875
bartlomiejwitek <bartlomiej.witek98@gmail.com>
2022-01-02 18:00:03 +0100

    Show matches and result GUI improvement
    Improvements

--- featureBased.py
+++ featureBased.py
@@ -1,6 +1,22 @@
+import math
+
 import cv2
 import numpy as np
 import os
+from dataclasses import dataclass
+
+@dataclass
+class ClassficationResult:
+    success: bool
+    resultString: str
+    matches: []
+    kp1: []
+    kp2: []
+    img1: object
+    accuracy: str
+
+
+
 
 
 class FeatureBasedClassifier:
@@ -10,7 +26,11 @@ class FeatureBasedClassifier:
         self.sift = cv2.SIFT_create()
         self.images = []
         self.classNames = []
-
+        self.kp1 = []
+        self.kp2 = []
+        self.matches = []
+        self.allMatchCount = 0
+        self.goodMatchList = []
 
     def loadImageClasses(self):
         #Loading image classes
@@ -20,6 +40,7 @@ class FeatureBasedClassifier:
         # classNames = []
 
         classList = os.listdir(path)
+        classList.remove(".DS_Store")
         print('Classes detected: ',len(classList))
 
         for imageClass in classList:
@@ -34,12 +55,14 @@ class FeatureBasedClassifier:
         for image in imageList:
             kp,des = self.sift.detectAndCompute(image,None)
             descriptorList.append(des)
+            self.kp1.append(kp)
         return descriptorList
 
     #classify the image to the class
     def getId(self,image, descriptorList):
         #find descriptor of the image to be classified
         kp,des = self.sift.detectAndCompute(image,None)
+        self.kp2.append(kp)
         bruteForceMatcher = cv2.BFMatcher()
         matchList = []
         imageClassifiedToIndex = -1
@@ -47,27 +70,70 @@ class FeatureBasedClassifier:
         try:
             for descriptor in descriptorList:
                 bruteForceMatches = bruteForceMatcher.knnMatch(descriptor,des, k=2)
+                bruteForceMatches2 = bruteForceMatcher.match(descriptor,des)
                 goodMatches = []
                 for i,j in bruteForceMatches:
                     if i.distance < 0.75 * j.distance:
-                        goodMatches.append([i])
+                        goodMatches.append(i)
                 matchList.append(len(goodMatches))
+                self.allMatchCount+= len(goodMatches)
+                self.matches.append(goodMatches)
         except:
             pass
         if len(matchList) != 0:
             if max(matchList) > minNumberOfMatches:
                 imageClassifiedToIndex = matchList.index(max(matchList))
         print(matchList)
+        self.goodMatchList.append(matchList)
+
         return imageClassifiedToIndex
+
+    def getAccuracy(self, id):
+        mean = self.allMatchCount / len(self.goodMatchList[0])
+        sum = 0
+        for matchCount in self.goodMatchList[0]:
+            sum += pow((matchCount - mean),2)
+
+        deviation = math.sqrt(sum/self.allMatchCount)
+
+        accuracy = self.goodMatchList[0][id] - mean
+        accurayDividedByDeviation = accuracy / deviation
+
+        return accurayDividedByDeviation
+
+    def cleanup(self):
+        self.kp1 = []
+        self.kp2 = []
+        self.matches = []
+        self.allMatchCount = 0
+        self.goodMatchList = []
 
     def classify(self, image):
         descriptorList = self.getDescriptors(self.images)
-        print(len(descriptorList))
+        # print(len(descriptorList))
         id = self.getId(image, descriptorList)
+        # TEST = cv2.drawMatches(self.images[id],self.kp1[id],image,self.kp2[0],self.matches[id],None);
+        # cv2.imshow("test", self.images[id])
+        # cv2.imshow("test2", image)
+        # cv2.imshow('matches', TEST)
         if id != -1:
-            return "Result: " + self.classNames[id]
+            accuracy= str(round(self.getAccuracy(id),2))
+            result = ClassficationResult(success=True,
+                                         resultString="Result: " + self.classNames[id],
+                                         matches=self.matches[id],
+                                         kp1=self.kp1[id],
+                                         kp2=self.kp2[0],
+                                         img1=self.images[id],
+                                         accuracy=str(accuracy)
+                                         )
+            self.cleanup()
+            return result
         else:
-            return 'Unknown'
+            result = ClassficationResult(success=False,resultString="Result: Unknown")
+            self.cleanup()
+            return result
+
+
             # cv2.putText(img2, "Result:" + classNames[id], (30, 30), cv2.QT_FONT_NORMAL, 1, (0, 0, 255), 1)
 
 
--- gui.py
+++ gui.py
@@ -32,32 +32,44 @@ def loadImage(canvas, img_id):
     canvas.itemconfig(img_id, image=imgtk)
     print(imgtk)
 
-def classifySift(canvas, img_id):
+def classifySift(canvas, img_id, img_right_id, match_label):
 
     result = featureBasedClassifier.classify(imageToClassifyPure)
     global imgtk2
-    cv2.putText(imageToDisplay,"Result:" + result,(20,50),cv2.QT_FONT_NORMAL,1,(255,0,10),2)
-    im = Image.fromarray(imageToDisplay)
+    cv2.putText(imageToDisplay,result.resultString,(20,50),cv2.QT_FONT_NORMAL,1,(255,0,10),2)
+    image = cv2.drawMatches(result.img1,result.kp1,imageToClassifyPure,result.kp2,result.matches,None)
+    dim = (1000,550)
+    image = cv2.resize(image, dim, interpolation=cv2.INTER_LINEAR)
+    im = Image.fromarray(image)
     imgtk2 = ImageTk.PhotoImage(image=im)
     canvas.itemconfig(img_id, image=imgtk2)
+    match_label.config(text="Best match: " + result.accuracy + " times the standard deviation")
+    # canvas.itemconfig(img_right_id, image=imgtk2)
 
 
 
 root = tk.Tk()
-canvas = tk.Canvas(root, height=600, width=500)
+canvas = tk.Canvas(root, height=600, width=1000)
 canvas.pack()
 img_id = canvas.create_image((0,0), image=None, anchor='nw')
+img_right_id= canvas.create_image((500,0), image=None, anchor='nw')
 
 
 # frame = tk.Frame(root, bg="black")
 # frame.place(relwidth=0.8, relheight=0.8, relx=0.1)
+my_label = tk.Label(root, text = "Results:")
+match_label = tk.Label(root, text = "Best match: ")
+
+my_label.pack()
+match_label.pack()
+
 
 openFile = tk.Button(root,text="Load Image", padx=10,pady=5, fg="black",
                      command=lambda: loadImage(canvas,img_id))
 openFile.pack();
 
 classifyImage= tk.Button(root, text="Classify Image SIFT",padx=10,pady=5, fg="black",
-                         command=lambda: classifySift(canvas,img_id))
+                         command=lambda: classifySift(canvas,img_id, img_right_id,match_label))
 classifyImage.pack()
 classifyImageCnn= tk.Button(root, text="Classify Image CNN",padx=10,pady=5, fg="black")
 classifyImageCnn.pack()
